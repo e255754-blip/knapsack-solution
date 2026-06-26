6b33aa8e2166ce56d1d17ff4a526a47bdd902976
e255754-blip <e255754@ie.u-ryukyu.ac.jp>
2026-06-26 17:41:23 +0900

    git

--- knapsack.py
+++ knapsack.py
@@ -10,23 +10,26 @@ best_total_capacity = 0
 n = len(items) 
 
 for i in range(2**n):
-    pattern = format(i, f"0{n}b")
+    #２進数に変換
+    pattern = (i, f"0{n}b")
     
     now_capacity = 0
     now_value = 0
     now_goods = []
     
+    #n回目のパターンの値段，容量，商品リストの作成
     for j in range(n):
         if pattern[j] == "1":
             now_capacity += goods[j]
             now_value += prices[j]
             now_goods.append(items[j])
             
+    #比較　　容量を超えてないかつ，一回前より値段が高かったら更新
     if now_capacity <= max_capacity and now_value > best_value:
         best_value = now_value
         best_total_capacity = now_capacity
         best_goods = now_goods
 
-print(f"選んだ品物の組み合わせ (番号): {best_goods}")
-print(f"総値段 (最大価値): {best_value}")
-print(f"総容量: {best_total_capacity} / {max_capacity}")
+print(f"選んだ品物: {best_goods}")
+print(f"最大の値段: {best_value}")
+print(f"容量: {best_total_capacity} / {max_capacity}")
